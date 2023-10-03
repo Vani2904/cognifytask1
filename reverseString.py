5ed6337a6fe26b95fa4903d6f376ab528e4dfc75
Vani2904 <142568607+Vani2904@users.noreply.github.com>
2023-10-03 22:04:43 +0530

    Add files via upload

---
+++ reverseString.py
@@ -0,0 +1,15 @@
+def reverse_string(input):
+    str = input[::-1]
+    return str
+
+
+
+input= "hello"
+
+str = reverse_string(input)
+
+print("The original string is : ")
+print(input)
+
+print("The reversed string is : ")
+print(str)
